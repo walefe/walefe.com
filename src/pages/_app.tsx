6bf55c50e782faf7b66fe0951bcf874fad853211
Walefe <walefedev@gmail.com>
2020-11-23 15:24:36 -0300

    feat: config tw for use tailwind and styled-compontens with netxjs.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from 'next/app';
+import { GlobalStyles } from 'twin.macro';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp;
